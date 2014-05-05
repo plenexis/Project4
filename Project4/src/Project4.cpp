5a939f4825f6f74edaf3aa8f41071a1fcf322553
Borjan Mukaetov <borjanm@t-home.mk>
2014-05-05 14:14:17 +0200

    Initial checkin of First C++ Hello World

---
+++ Project4/src/Project4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Project4.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Borjan Mukaetov" << endl; // prints !!!Hello World!!!
+	return 0;
+}
